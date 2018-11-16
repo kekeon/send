d792bd2daeff2deee18d4bd35ec4b2721f50b9e7
kekeon <liuke095@gmail.com>
2018-11-17 00:26:48 +0800

    add invite page

--- src/components/Operation.js
+++ src/components/Operation.js
@@ -16,18 +16,20 @@ export default class Operation extends React.Component {
     render() {
         const { value } = this.state
         return (
-            <section id="create-project">
-            <Form>
-                <Form.Group widths='equal'>
-                    <Form.Input fluid label='项目名称' placeholder='First name' />
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    <Form.Select fluid label='选择类别' options={options} placeholder='Gender' />
-                </Form.Group>
-                <Form.TextArea label='项目描述' placeholder='介绍一下项目吧' />
-                <Form.Checkbox label='我会遵守send使用条约' />
-                <Form.Button>创建</Form.Button>
-            </Form>
+            <section id="create-project" class="main-page">
+                <div class="page-box">
+                    <Form>
+                        <Form.Group widths='equal'>
+                            <Form.Input fluid label='项目名称' placeholder='First name' />
+                        </Form.Group>
+                        <Form.Group widths='equal'>
+                            <Form.Select fluid label='选择类别' options={options} placeholder='Gender' />
+                        </Form.Group>
+                        <Form.TextArea label='项目描述' placeholder='介绍一下项目吧' />
+                        <Form.Checkbox label='我会遵守send使用条约' />
+                        <Form.Button>创建</Form.Button>
+                    </Form>
+                </div>
             </section>
         )
     }
--- src/components/OperationRecord.js
+++ src/components/OperationRecord.js
@@ -1,12 +1,34 @@
 
 import React from 'react'
+import { Form, TextArea, Button,Header, Icon,Container } from 'semantic-ui-react'
 
 
 export default class OperationRecord extends React.Component {
+//envelope outline
 
     render(){
         return(
-            <div>OperationRecord</div>
+            <section class="main-page" id="invite">
+                <div class="page-box">
+                    <Form>
+                        <Header as='h5'>
+                            <Icon name='envelope outline' />
+                            <Header.Content>邀请战友</Header.Content>
+                        </Header>
+                        <Form.Field
+                            id='form-textarea-control-opinion'
+                            control={TextArea}
+                            placeholder='请输入战友email...,使用 ‘ / ’ 隔开'
+                        />
+                        <Container textAlign='left' className="msg">快和你的战友，抓紧开始吧......</Container>
+                        <Form.Field
+                            id='form-button-control-public'
+                            control={Button}
+                            content='发送邀请'
+                        />
+                    </Form>
+                </div>
+            </section>
         )
     }
 }

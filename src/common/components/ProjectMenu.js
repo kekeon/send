78358e90bbab47788b0eb30d95507ec349481b4c
kekeon <liuke095@gmail.com>
2018-11-19 23:59:14 +0800

    create project menu

---
+++ src/common/components/ProjectMenu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class ProjectMenu extends React.Component {
+    render() {
+        return (
+            <section id="project-menu">
+                <ul>
+                    <li>Rename</li>
+                    <li>Edit</li>
+                    <li>Delete</li>
+                    <li>Add Folder</li>
+                </ul>
+            </section>
+        )
+    }
+}

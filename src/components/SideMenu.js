9f57cc33c3ece4b961b7cb9081b7d93f53c5837f
kekeon <liuke095@gmail.com>
2018-11-20 23:51:40 +0800

    add tool menu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import ToolMenu from '../common/components/ToolMenu'
 export default  class SideMenu extends  React.Component {
     state = { visible: false }
 
@@ -24,7 +25,9 @@ export default  class SideMenu extends  React.Component {
                             </Button>
                         </Menu.Item>
                         <Menu.Item as={Link} to="/main/invite-member">邀请战友</Menu.Item>
-                        <Menu.Item as={Link} to="/main/project-menu"  className="clearfix">项目menu <Icon name="ellipsis horizontal float-right"></Icon></Menu.Item>
+                        <Menu.Item as={Link} to="/main/project-menu"  className="clearfix">项目menu <Icon name="ellipsis horizontal" className="float-right ellipsis-tool">
+                            <ToolMenu></ToolMenu>
+                        </Icon></Menu.Item>
                     </Sidebar>
                 </Sidebar.Pushable>
             </div>
